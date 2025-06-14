4e582c209e84d7a58fbcfe8764face39cde9f6ec
maxborey <maxborey@gmail.com>
2025-06-14 18:47:22 +0300

    finish_hw_5_0614_end

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import 'modern-normalize';
+import 'modern-normalize/modern-normalize.css';
+
 
 
 const queryClient = new QueryClient();
